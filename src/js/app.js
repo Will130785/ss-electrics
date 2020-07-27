e344438691eb0a369389ff620d44fe14da5657ba
willconstable <will_constable@msn.com>
2020-07-27 19:51:47 +0100

    Add navbar layout

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+//Toggle class on hamburger
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", e => {
+        hamburger.classList.toggle("is-active");
+    });
