0471c0a7298ecb650b3afd98f36eae61f26eca11
willconstable <will_constable@msn.com>
2020-07-29 18:42:07 +0100

    Add general home page layout

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,58 @@ const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", e => {
         hamburger.classList.toggle("is-active");
     });
+
+        // new Glide('.glide').mount();
+
+    // var input = document.querySelector('#options-autoplay-input')
+
+    new Glide('.options-autoplay-1', {
+      type: "carousel",
+      autoplay: 2000,
+      hoverpause: true,
+      perView: 4,
+      breakpoints: {
+        1100: {
+          perView: 3
+        },
+        900: {
+          perView: 2
+        },
+        600: {
+          perView: 1
+        }
+      }
+    }).mount();
+
+    new Glide('.options-autoplay-2', {
+      type: "carousel",
+      autoplay: 2000,
+      hoverpause: true,
+      perView: 6,
+      breakpoints: {
+        1100: {
+          perView: 3
+        },
+        900: {
+          perView: 2
+        },
+        600: {
+          perView: 1
+        }
+      }
+    }).mount();
+
+    new Glide('.options-autoplay-3', {
+      type: "slider",
+      autoplay: 2000,
+      hoverpause: true,
+      perView: 1
+    }).mount();
+    
+    // input.addEventListener('input', function (event) {
+    //   glide.update({
+    //     autoplay: (event.target.value != 0) ? event.target.value : false
+    //   })
+    // })
+    
+
